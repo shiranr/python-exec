aea6f06c1bbdb001819452f58e464aa7c785be79
shiranr <shiranrubin@gmail.com>
2023-01-31 18:01:19 +0200

    Add project toml to install the linter fix descriptor

--- linter/__init__.py
+++ linter/__init__.py
@@ -1,9 +1,9 @@
 """A Python CLI for receiving and running functions."""
 
 
-APP_NAME = "exec_cli"
+APP_NAME = "python-exec"
 APP_VERSION = "0.0.1"
 APP_DESCRIPTION = __doc__
-SHELL_NAME = "exec_py_cli"
+SHELL_NAME = "python-exec"
 
 __version__ = APP_VERSION
